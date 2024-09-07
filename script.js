f01727a0a45cf5f39c9001cc0c7d3aec41a13ecc
Vijaygowda09 <134143862+Vijaygowda09@users.noreply.github.com>
2024-09-07 13:33:46 +0530

    Update script.js

--- script.js
+++ script.js
@@ -4,7 +4,7 @@ let btnReset = $('#btnReset')
 let btnEdit = $('#btnEdit')
 let btnCleanup = $('#btnCleanup')
 let inpNewTask = $('#inpNewTask')
-let currentEditItem = null;
+let editingTask = null
 
 function addItem() {
   let listItem = $('<li>', {
@@ -12,9 +12,15 @@ function addItem() {
     text: inpNewTask.val()
   })
   listItem.click(() => {
-    ulTasks.children().removeClass('selected')  // Remove selection from all items
-    listItem.toggleClass('selected')  // Select clicked item
-    toggleInputButtons()  // Update buttons based on selection
+    listItem.toggleClass('done')
+    toggleInputButtons()
+  })
+  listItem.dblclick(() => {
+    if (ulTasks.children('.done').length === 1) {
+      editingTask = listItem
+      inpNewTask.val(listItem.text())
+      toggleInputButtons()
+    }
   })
   ulTasks.append(listItem)
   inpNewTask.val('')
@@ -22,57 +28,38 @@ function addItem() {
 }
 
 function clearDone() {
-  $('#ulTasks .done').fadeOut(function() {
-    $(this).remove()
+  $('#ulTasks .done').each(function() {
+    $(this).remove()  // Directly remove the done tasks
   })
   toggleInputButtons()
 }
 
-function editItem() {
-  let selectedItem = $('#ulTasks .selected')
-  if (selectedItem.length === 1) {
-    currentEditItem = selectedItem;
-    inpNewTask.val(selectedItem.text())  // Set input field to the selected item's text for editing
+function editTask() {
+  if (editingTask) {
+    editingTask.text(inpNewTask.val())
+    inpNewTask.val('')
+    editingTask = null
+    toggleInputButtons()
   }
 }
 
 function toggleInputButtons() {
   btnReset.prop('disabled', inpNewTask.val() === '')
   btnAdd.prop('disabled', inpNewTask.val() === '')
-  btnEdit.prop('disabled', ulTasks.children('.selected').length !== 1)  // Enable only if one item is selected
+  btnEdit.prop('disabled', ulTasks.children('.done').length !== 1)
   btnCleanup.prop('disabled', ulTasks.children().length < 1)
 }
 
 inpNewTask.keypress((e) => {
-  if (e.which == 13 && currentEditItem) {  // Press Enter to finish editing
-    currentEditItem.text(inpNewTask.val())
-    currentEditItem.removeClass('selected')
-    currentEditItem = null
-    inpNewTask.val('')
-    toggleInputButtons()
-  } else if (e.which == 13) {
-    addItem()
-  }
+  if (e.which == 13) addItem()
 })
 
 inpNewTask.on('input', toggleInputButtons)
 
-btnAdd.click(() => {
-  if (currentEditItem) {
-    currentEditItem.text(inpNewTask.val())  // Update the item text when editing
-    currentEditItem.removeClass('selected')
-    currentEditItem = null
-  } else {
-    addItem()
-  }
-  inpNewTask.val('')
-  toggleInputButtons()
-})
-
+btnAdd.click(addItem)
 btnReset.click(() => {
   inpNewTask.val('')
   toggleInputButtons()
 })
-
+btnEdit.click(editTask)
 btnCleanup.click(clearDone)
-btnEdit.click(editItem)
