5a6a7d6f02e331b4d203d1f51351145dfc5fa476
Vijaygowda09 <134143862+Vijaygowda09@users.noreply.github.com>
2024-09-07 13:26:15 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,9 +1,10 @@
 let ulTasks = $('#ulTasks')
 let btnAdd = $('#btnAdd')
 let btnReset = $('#btnReset')
-let btnSort = $('#btnSort')
+let btnEdit = $('#btnEdit')
 let btnCleanup = $('#btnCleanup')
 let inpNewTask = $('#inpNewTask')
+let currentEditItem = null;
 
 function addItem() {
   let listItem = $('<li>', {
@@ -11,7 +12,9 @@ function addItem() {
     text: inpNewTask.val()
   })
   listItem.click(() => {
-    listItem.toggleClass('done')
+    ulTasks.children().removeClass('selected')  // Remove selection from all items
+    listItem.toggleClass('selected')  // Select clicked item
+    toggleInputButtons()  // Update buttons based on selection
   })
   ulTasks.append(listItem)
   inpNewTask.val('')
@@ -25,27 +28,51 @@ function clearDone() {
   toggleInputButtons()
 }
 
-function sortTasks() {
-  $('#ulTasks .done').appendTo(ulTasks)
+function editItem() {
+  let selectedItem = $('#ulTasks .selected')
+  if (selectedItem.length === 1) {
+    currentEditItem = selectedItem;
+    inpNewTask.val(selectedItem.text())  // Set input field to the selected item's text for editing
+  }
 }
 
 function toggleInputButtons() {
   btnReset.prop('disabled', inpNewTask.val() === '')
   btnAdd.prop('disabled', inpNewTask.val() === '')
-  btnSort.prop('disabled', ulTasks.children().length < 1)
+  btnEdit.prop('disabled', ulTasks.children('.selected').length !== 1)  // Enable only if one item is selected
   btnCleanup.prop('disabled', ulTasks.children().length < 1)
 }
 
 inpNewTask.keypress((e) => {
-  if (e.which == 13) addItem()
+  if (e.which == 13 && currentEditItem) {  // Press Enter to finish editing
+    currentEditItem.text(inpNewTask.val())
+    currentEditItem.removeClass('selected')
+    currentEditItem = null
+    inpNewTask.val('')
+    toggleInputButtons()
+  } else if (e.which == 13) {
+    addItem()
+  }
 })
 
 inpNewTask.on('input', toggleInputButtons)
 
-btnAdd.click(addItem)
+btnAdd.click(() => {
+  if (currentEditItem) {
+    currentEditItem.text(inpNewTask.val())  // Update the item text when editing
+    currentEditItem.removeClass('selected')
+    currentEditItem = null
+  } else {
+    addItem()
+  }
+  inpNewTask.val('')
+  toggleInputButtons()
+})
+
 btnReset.click(() => {
   inpNewTask.val('')
   toggleInputButtons()
 })
+
 btnCleanup.click(clearDone)
-btnSort.click(sortTasks)
+btnEdit.click(editItem)
